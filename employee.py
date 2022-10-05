18bb603b29ca3586ce35a188d0ad0116ca956fd3
angelalee0428 <angela.seongjoolee@gmail.com>
2022-10-05 00:32:08 -0600

    UnitTesting

--- employee.py
+++ employee.py
@@ -5,7 +5,7 @@ class Employee:
 
     raise_amt = 1.05
 
-    def __intit__(self, first, last, pay):
+    def __init__(self, first, last, pay):
         self.first = first
         self.last = last
         self.pay = pay
@@ -16,7 +16,7 @@ class Employee:
 
     @property
     def fullname(self):
-        return '{}.{}'.format(self.first, self.last)
+        return '{} {}'.format(self.first, self.last)
 
     def apply_raise(self):
         self.pay = int(self.pay * self.raise_amt)
